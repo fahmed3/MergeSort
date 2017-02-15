2570573e72b8a3b3a28a95dec162e543f5c38415
fahmed3 <fahmed3@stuy.edu>
2017-02-14 21:05:14 -0500

    MergeSort and tester

---
+++ MergeSortTester.java
@@ -0,0 +1,55 @@
+/*======================================
+  class MergeSortTester
+
+  ALGORITHM:
+  <INSERT YOUR DISTILLATION OF ALGO HERE>
+
+  BIG-OH CLASSIFICATION OF ALGORITHM:
+  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
+
+  Mean execution times for dataset of size n:
+  Batch size: <# of times each dataset size was run>
+  n=1       time: 
+  n=10      time: 
+  n=100     time: 
+  ...
+  n=<huge>  time: 
+
+  ANALYSIS:
+  <INSERT YOUR RESULTS ANALYSIS HERE>
+  ======================================*/
+
+public class MergeSortTester 
+{
+
+    /******************************
+     * execution time analysis 
+     * <INSERT YOUR DESCRIPTION HERE OF 
+     *  YOUR APPARATUS FOR GENERATING EXECUTION 
+     *  TIME DATA...>
+     ******************************/
+    public static void main( String[] args ) 
+    {
+	MergeSort a = new MergeSort();
+	
+	long time = System.currentTimeMillis();
+	int[] arr0 = {0};
+	int[] arr1 = {1};
+	int[] arr2 = {1,2};
+	int[] arr3 = {3,4};
+	int[] arr4 = {1,2,3,4};
+	int[] arr5 = {4,3,2,1};
+	int[] arr6 = {9,42,17,63,0,512,23};
+	int[] arr7 = {9,42,17,63,0,9,512,23,9};
+
+	System.out.println("\nSorting arr4-7...");
+	a.printArray( a.sort( arr4 ) );
+	a.printArray( a.sort( arr5 ) );
+	a.printArray( a.sort( arr6 ) );
+	a.printArray( a.sort( arr7 ) );
+
+	time = System.currentTimeMillis() - time;
+	System.out.println("Time (ms): " + time);
+    }//end main
+
+}//end class
